95009e87d7db4379de241c75ee7e01a9e04900d6
Adam Meily <meily.adam@gmail.com>
2015-01-13 17:55:08 -0500

    improvements

--- market.js
+++ market.js
@@ -2,30 +2,46 @@
 var Stock = require('./stock');
 var EventEmitter = require('events').EventEmitter;
 var GitInterface = require('./git');
+var StockController = require('./stock');
+var Stock = require('./models').stock;
 
 
-module.exports = function(attrs) {
+
+var MarketController = function(attrs) {
     EventEmitter.call(this);
 
-    this.path = attrs.path || "";
-    this.name = attrs.name || "";
-    this.parent = attrs.parent || null;
-    this.stocks = attrs.stocks || { };
-    this.plugins = attrs.plugins || [];
-    this.git = new GitInterface(this.path);
+    this.market = attrs.market;
+    this.stocks = attrs.stocks || [];
+    this.git = new GitInterface({
+        path: this.market.path
+    });
 
-    this.processCommit = function(commit) {
+    this.initialize = function(cb) {
         var self = this;
-        _.each(this.plugins, function(plugin) {
-            plugin.processCommit(commit, self);
+
+        // Get Stocks
+        Stock.find({ market: this.market._id }, function(err, stocks) {
+            _.each(stocks, function(stock) {
+                self.stocks.push(stock);
+                self.emit("stock", stock);
+            });
+            if(cb) cb();
         });
     };
 
-    this.processRange = function(before, after) {
+    this.processCommit = function(hash) {
+        var self = this;
+        this.git.getCommit(hash, function(commit) {
+            self.emit("commit", commit);
+        });
+    };
+
+    this.processCommitRange = function(before, after) {
         var self = this;
 
-        this.git.parseCommitList(before, after, function(commit) {
-            self.processCommit(commit);
+        this.git.getCommitList(before, after, function(commit) {
+            commit.stock = self.getStockByEmail(commit.author, true);
+            self.emit("commit", commit);
         });
     };
 
@@ -34,41 +50,39 @@ module.exports = function(attrs) {
         if(id in this.stocks) {
             return this.stocks[id];
         } else if(create) {
-            var stock = this.stocks[id] = new Stock({
+            var stock = new Stock({
                 email: user.email,
                 name: user.name,
-                market: this;
+                market: this._id,
+                price: 0
             });
 
-            this.emit("newStock", stock);
+            this.emit("stock", stock);
             return stock;
         }
         return null;
     };
 
     this.post = function(evt) {
-        if(!evt.user) {
-            return;
+        /*
+        {
+            target: <commit>,
+            stock: <stock>,
+            shares: +/-,
+            price: +/-
+        }
+        */
+        var value = this.market.value;
+        if(evt.shares) {
+
         }
 
-        var self = this;
-        var users = _.isArray(evt.user) ? evt.user : [evt.user];
-        _.each(users, function(user) {
-            var stock = self.getStockByEmail(user, true);
-            stock.value += evt.value;
-            self.emit("delta", { stock: stock, tick: tick });
-        });
+        if(evt.price) {
 
-        this.emit("post", evt);
-    };
+        }
 
-    this.getGdp = function() {
-        var total = 0;
-        _.each(this.stocks, function(stock) {
-            total += stock.value;
-        });
-        return total;
+        this.emit('post', evt, this);
     };
 };
 
-
+export.MarketController = MarketController;
