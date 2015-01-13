dd620caef1d650686fc30ca57b3856a1b76c70ae
Adam Meily <meilya@ainfosec.com>
2015-01-13 00:18:44 -0500

    added commit model and day summaries

--- models.js
+++ models.js
@@ -1,30 +1,13 @@
 
 var mongoose = require('mongoose');
+var moment = require('moment');
 
-
-var MarketSchema = new mongoose.Schema({
-    groups: [String],
-    path: String,
-    name: String,
-    value: Number,
-    symbol: String
-});
-export.Market = mongoose.Model('Market', MarketSchema);
-
-
-var StockSchema = new mongoose.Schema({
-    email: String,
-    market: {
-        type: mongoose.Schema.ObjectId,
-        ref: "Market"
-    },
-    price: Number
-});
-export.Stock = mongoose.Model('Stock', StockSchema);
-
-
-
-
+function getDayTimestamp(ts) {
+    if(ts === undefined) {
+        ts = moment().unix();
+    }
+    return ts - (ts % 86400);
+}
 
 var StockDaySummary = mongoose.Model('StockDaySummary', {
     stock: {
@@ -40,7 +23,78 @@ var MarketDaySummary = mongoose.Model('MarketDaySummary', {
         type: mongoose.Schema.ObjectId,
         ref: 'Market'
     },
-    price: Number,
+    value: Number,
     timestamp: Number
 });
+
+
+var MarketGroup = mongoose.Model('MarketGroup', {
+    symbol: String,
+    name: String
+});
+
+
+var MarketSchema = new mongoose.Schema({
+    groups: [String],
+    path: String,
+    name: String,
+    value: Number,
+    symbol: String
+});
+
+MarketSchema.methods.getSummaryForToday = function(cb) {
+    var market = this;
+    var ts = getDayTimestamp();
+
+    MarketDaySummary.findOneAndUpdate({ market: this._id, timestamp: ts }, {
+        $setOnInsert: {
+            stock: this._id,
+            timestamp: ts,
+            value: market.value
+        }
+    }, { new: true, upsert: true }, function() {
+        cb(this, market);
+    });
+};
+
+export.Market = mongoose.Model('Market', MarketSchema);
+
+
+var StockSchema = new mongoose.Schema({
+    email: String,
+    market: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Market"
+    },
+    price: Number
+});
+
+StockSchema.methods.getSummaryForToday = function(cb) {
+    var stock = this;
+    var ts = getDayTimestamp();
+
+    StockDaySummary.findOneAndUpdate({ stock: this._id, timestamp: ts }, {
+        $setOnInsert: {
+            stock: this._id,
+            timestamp: ts,
+            price: stock.price
+        }
+    }, { new: true, upsert: true }, function() {
+        cb(this, stock);
+    });
+};
+export.Stock = mongoose.Model('Stock', StockSchema);
+
+
+var CommitSchema = new mongoose.Schema({
+    hash: String,
+    branches: [String],
+    market: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Market"
+    }
+});
+export.Commit = mongoose.Model('Commit', CommitSchema);
+
+
 
