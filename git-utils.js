1401f111d74e699c343adfd4033b216f13968363
Adam Meily <meilya@ainfosec.com>
2015-10-13 18:58:55 -0400

    initial commit of git utility functions

---
+++ git-utils.js
@@ -0,0 +1,42 @@
+
+var git = require('nodegit');
+
+
+function testFlag(value, flag) {
+  return (value & flag) == flag;
+}
+
+function getDiffFile(delta) {
+  return delta.newFile().id().iszero() ? delta.oldFile() : delta.newFile();
+}
+
+function isDeltaBinaryFile(delta) {
+  var b = isDeltaNormalFile(delta);
+  return b && testFlag(getDiffFile(delta).flags(), git.Diff.FLAG.BINARY);
+}
+
+
+function isDeltaNormalFile(delta) {
+  var df = getDiffFile(delta);
+  return testFlag(df.mode(), git.TreeEntry.FILEMODE.BLOB);
+}
+
+
+function isDeltaNewFile(delta) {
+  return delta.oldFile().id().iszero() == 1 &&
+    delta.newFile().id().iszero() == 0 &&
+    testFlag(delta.status(), git.Diff.DELTA.ADDED);
+}
+
+function isDeltaDeletedFile(delta) {
+  return delta.oldFile().id().iszero() == 0 &&
+    delta.newFile().id().iszero() == 1 &&
+    testFlag(delta.status(), git.Diff.DELTA.DELETED);
+}
+
+exports.getDiffFile = getDiffFile;
+exports.isDeltaNormalFile = isDeltaNormalFile;
+exports.isDeltaBinaryFile = isDeltaBinaryFile;
+exports.isDeltaNewFile = isDeltaNewFile;
+exports.isDeltaDeletedFile = isDeltaDeletedFile;
+exports.testFlag = testFlag;
