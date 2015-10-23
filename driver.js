d5f4109bb7a76bf5837b187e146e61de1d843b0b
Adam Meily <meilya@ainfosec.com>
2015-10-22 20:00:03 -0500

    add stock tracking

--- driver.js
+++ driver.js
@@ -4,6 +4,9 @@ var Promise = require('promise');
 var util = require('util');
 var _ = require('underscore');
 var PluginPipeline = require('./pipeline');
+var Stock = require('./stock');
+var ansi = require('ansi');
+var cursor = ansi(process.stdout);
 
 
 function MarketDriver(options) {
@@ -13,6 +16,7 @@ function MarketDriver(options) {
   this.repo = null;
   this.stocks = {};
   this.commits = 0;
+  this.commitHistory = {};
 }
 
 
@@ -31,7 +35,10 @@ MarketDriver.prototype.handleSingle = function(hash, branch) {
     branch: branch
   };
 
-  console.log("Processing commit:", hash);
+  cursor
+    .horizontalAbsolute(0)
+    .eraseLine()
+    .write("Processing Commit: " + hash.toString());
 
   return this.repo.getCommit(hash).then(function(commit) {
     obj.commit = commit;
@@ -50,6 +57,7 @@ MarketDriver.prototype.handleSingle = function(hash, branch) {
   }).then(function() {
     return self.makePipeline(obj);
   }).catch(function(error) {
+    console.log();
     //console.log("handleSingle error: ", error); // TODO
     throw error;
   });
@@ -81,26 +89,30 @@ MarketDriver.prototype.makePipeline = function(obj) {
   var self = this;
 
   var promise = new Promise(function(resolve, reject) {
+    var email = obj.commit.author().email();
+    var stock;
+    if(email in self.stocks) {
+      stock = self.stocks[email];
+    } else {
+      stock = self.stocks[email] = new Stock(email);
+    }
+
     var pipe = new PluginPipeline({
       branch: obj.branch,
       commit: obj.commit,
       market: null,
       config: self.config,
       plugins: self.plugins,
-      diffList: obj.diffList
+      diffList: obj.diffList,
+      stock: stock
     });
 
     pipe.on('end', function() {
-      var email = obj.commit.author().email();
-      if(email in self.stocks) {
-        self.stocks[email] += pipe.delta;
-      } else {
-        self.stocks[email] = pipe.delta;
-      }
       self.commits += 1;
       resolve();
     }).on('error', function(err) {
-      console.log("rejected:", reject);
+      console.log("rejected");
+      console.trace(err);
       reject(err);
     }).next();
   });
